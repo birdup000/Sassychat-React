5f3899f26aa93bd8b63b43b243d5efc5cb896cf1
Bullseyecowbelly <34012548+greengeckowizard@users.noreply.github.com>
2023-04-07 19:51:23 -0500

    Update App.js

--- sassychat/src/App.js
+++ sassychat/src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
-import {Person, Avatar} from './UserProfile'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { Person, Avatar } from './UserProfile';
+import VideoChatPage from './VideoChatPage';
 
 function App() {
-  let nate = new Person("nate", "VWjRf", "8tyr", 11)
+  let nate = new Person("nate", "VWjRf", "8tyr", 11);
   return (
-    <div className="App">
-      <header className="Main-Header">
-        <Avatar person = {nate.Person} size='1' />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="Main-Header">
+          <Avatar person={nate.Person} size="1" />
+        </header>
+        <Link to="/video-chat">
+          <button>Go to Video Chat</button>
+        </Link>
+        <Switch>
+          <Route path="/video-chat" component={VideoChatPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
